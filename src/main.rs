1a6693041dcb94dde48d22068bb76fe18687f946
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-14 16:20:06 -0400

    vectors

--- src/main.rs
+++ src/main.rs
@@ -7,35 +7,21 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    enum Day {
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday,
-        Sunday,
+    let vec1: Vec<i32> = Vec::new();
+    let mut vec2 = vec![1, 2, 3, 4];
+    vec2.push(5);
+    println!("1st : {}", vec2[0]);
+    let second: &i32 = &vec2[1];
+    match vec2.get(1) {
+        Some(second) => println!("2nd : {}", second),
+        None => println!("No 2nd value"),
     }
-
-    impl Day {
-        fn is_weekend(&self) -> bool {
-            match self {
-                Day::Saturday | Day::Sunday => true,
-                _ => false,
-            }
-        }
+    for i in &mut vec2 {
+        *i *= 2;
     }
-
-    let today: Day = Day::Monday;
-    match today {
-        Day::Monday => println!("Everyone hates Mondays"),
-        Day::Tuesday => println!("Taco Tuesday"),
-        Day::Wednesday => println!("Hump Day"),
-        Day::Thursday => println!("Pay Day"),
-        Day::Friday => println!("Almost weekend"),
-        Day::Saturday => println!("Weekend"),
-        Day::Sunday => println!("Weekend"),
+    for i in &vec2 {
+        println!("{}", i)
     }
-
-    println!("Is today the weekend? {}", today.is_weekend())
+    println!("Vec Length {}", vec2.len());
+    println!("Pop : {:?}", vec2.pop());
 }
