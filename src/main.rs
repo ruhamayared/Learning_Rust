576fd0721593dabdb145a4920e8178ee4b9ce909
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-14 16:11:13 -0400

    enum types

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,35 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let st3 = String::from("x r t b h k k a m c");
-    let mut v1: Vec<char> = st3.chars().collect();
-    v1.sort();
-    v1.dedup();
-    for char in v1 {
-        println!("{}", char);
+    enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
     }
 
-    let st4: &str = "Random string";
-    let mut st5: String = st4.to_string();
-    println!("{}", st5);
-    let byte_arr1 = st5.as_bytes();
-    let st6 = &st5[0..6];
-    println!("String length: {}", st6.len());
-    st5.clear();
-
-    let st6 = String::from("Just some");
-    let st7 = String::from("words.");
-    let st8 = st6 + &st7;
-    for char in st8.bytes() {
-        println!("{}", char);
+    impl Day {
+        fn is_weekend(&self) -> bool {
+            match self {
+                Day::Saturday | Day::Sunday => true,
+                _ => false,
+            }
+        }
     }
+
+    let today: Day = Day::Monday;
+    match today {
+        Day::Monday => println!("Everyone hates Mondays"),
+        Day::Tuesday => println!("Taco Tuesday"),
+        Day::Wednesday => println!("Hump Day"),
+        Day::Thursday => println!("Pay Day"),
+        Day::Friday => println!("Almost weekend"),
+        Day::Saturday => println!("Weekend"),
+        Day::Sunday => println!("Weekend"),
+    }
+
+    println!("Is today the weekend? {}", today.is_weekend())
 }
