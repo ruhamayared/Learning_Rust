b203e6713d0ad896f108a6377301e0f1d9b7f108
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-14 14:18:12 -0400

    if statements

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,26 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    const ONE_MIL: u32 = 1_000_000;
-    const PI: f32 = 3.141592;
-    let age = "47";
-    let mut age: u32 = age.trim().parse().expect("Age wasn't assigned a number.");
-    age = age + 1;
-    println!("I'm {} and I want ${}.", age, ONE_MIL)
+    let age = 20;
+    if (age >= 1) && (age <= 18) {
+        println!("Important Birthday!")
+    } else if (age == 21) || (age == 50) {
+        println!("Important Birthday!")
+    } else if age >= 65 {
+        println!("Important Birthday!")
+    } else {
+        println!("Not an important birthday 😪.")
+    }
+
+    let mut my_age = 47;
+    let can_vote = if my_age >= 18 { true } else { false };
+    println!("Can Vote: {}", can_vote);
+
+    let age2 = 8;
+    match age2 {
+        1..=18 => println!("Important Birthday!"),
+        21 | 50 => println!("Important Birthday!"),
+        65..=i32::MAX => println!("Important Birthday!"),
+        _ => println!("Not an important birthday 😪."),
+    };
 }
