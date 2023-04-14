80583bc492492389872a88fa6d0510c6a2bb1bdb
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-14 14:20:49 -0400

    match

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,11 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let age = 20;
-    if (age >= 1) && (age <= 18) {
-        println!("Important Birthday!")
-    } else if (age == 21) || (age == 50) {
-        println!("Important Birthday!")
-    } else if age >= 65 {
-        println!("Important Birthday!")
-    } else {
-        println!("Not an important birthday 😪.")
-    }
-
-    let mut my_age = 47;
-    let can_vote = if my_age >= 18 { true } else { false };
-    println!("Can Vote: {}", can_vote);
-
-    let age2 = 8;
-    match age2 {
-        1..=18 => println!("Important Birthday!"),
-        21 | 50 => println!("Important Birthday!"),
-        65..=i32::MAX => println!("Important Birthday!"),
-        _ => println!("Not an important birthday 😪."),
+    let my_age = 18;
+    let voting_age = 18;
+    match my_age.cmp(&voting_age) {
+        Ordering::Less => println!("Can't vote 😪."),
+        Ordering::Greater => println!("Can vote 🤩."),
+        Ordering::Equal => println!("You gained the right to vote 🤩."),
     };
 }
