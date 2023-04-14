b25480b8a9c25311db3280ec0470f61fd519a804
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-14 13:50:21 -0400

    user input

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ use std::io::{BufRead, BufReader, ErrorKind, Write};
 fn main() {
     println!("What is your name?");
     let mut name = String::new();
-    let greeting: &str = "Nice to meet you";
+    let greeting: &str = "Nice to meet you!";
     io::stdin()
         .read_line(&mut name)
         .expect("Didn't Receive Input");
 
-    println!("Hello, world!");
+    println!("Hello, {}! {}", name.trim_end(), greeting);
 }
