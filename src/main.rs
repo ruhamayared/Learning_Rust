6a5f4fe4abc8aec546fbcc314598e31bddc31685
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-14 14:32:31 -0400

    while for loops

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,14 @@ use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
     let arr_1: [i32; 9] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    println!("1st: {}", arr_1[0]);
-    println!("length: {}", arr_1.len());
-
     let mut loop_idx: usize = 0;
-    loop {
-        if arr_1[loop_idx] % 2 == 0 {
-            loop_idx += 1;
-            continue;
-        }
-        if arr_1[loop_idx] == 9 {
-            break;
-        }
-        println!("Val: {}", arr_1[loop_idx]);
+
+    while loop_idx < arr_1.len() {
+        println!("Array: {}", arr_1[loop_idx]);
         loop_idx += 1;
+    }
+
+    for val in arr_1.iter() {
+        println!("Val: {}", val);
     }
 }
