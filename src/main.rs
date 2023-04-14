4c5b41453b285b83f67941f6aa35e3bee6e71016
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-14 13:47:25 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![allow(unused)]
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
+fn main() {
+    println!("What is your name?");
+    let mut name = String::new();
+    let greeting: &str = "Nice to meet you";
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Didn't Receive Input");
+
+    println!("Hello, world!");
+}
