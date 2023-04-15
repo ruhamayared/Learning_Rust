0d8a68e5a284eb25db2f3d9424b90fec103eb119
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-15 17:51:53 -0400

    closures

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,31 @@ mod restaurant;
 use crate::restaurant::order_food;
 
 fn main() {
-    let mut arr_it = [1, 2, 3, 4];
-    for val in arr_it.iter() {
-        println!("{}", val)
-    }
+    //let var_name = |parameters| -> return_type {body}
 
-    arr_it.into_iter();
+    let can_vote = |age: i32| age >= 18;
+    println!("Can vote: {}", can_vote(8));
 
-    let mut iter1 = arr_it.iter();
-    println!("1st: {:?}", iter1.next());
+    let mut samp1 = 5;
+    let print_var = || println!("samp1 = {}", samp1);
+    print_var();
+
+    samp1 = 10;
+    let mut change_var = || samp1 += 1;
+    change_var();
+    println!("samp1: {}", samp1);
+
+    samp1 = 10;
+    println!("samp1: {}", samp1);
+
+    fn use_func<T>(a: i32, b: i32, func: T) -> i32
+    where
+        T: Fn(i32, i32) -> i32,
+    {
+        func(a, b)
+    };
+    let sum = |a, b| a + b;
+    let prod = |a, b| a * b;
+    println!("5 + 4 = {}", use_func(5, 4, sum));
+    println!("5 * 4 = {}", use_func(5, 4, prod));
 }
