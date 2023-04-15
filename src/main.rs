76d75d93af47063cbbc1e6b8ce858ba0c5105040
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-15 10:23:30 -0400

    error handeling

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,40 @@ use std::io::{BufRead, BufReader, ErrorKind, Write};
 mod restaurant;
 use crate::restaurant::order_food;
 
+// Result has 2 varients Ok and Err
+// enum Result <T, E> {
+// Ok(T)
+// Err(E)}
+// Where T represents the type of data the value returns
+// And E represents the type of error
+
 fn main() {
-    order_food();
+    let path: &str = "lines.txt";
+    let output = File::create(path);
+    let mut output = match output {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Problem creating file: {:?}", error);
+        }
+    };
+    write!(output, "Just some\nRandom words.").expect("Failed to wirte to file.");
+
+    let input = File::open(path).unwrap();
+    let bufffered = BufReader::new(input);
+
+    for line in bufffered.lines() {
+        println!("{}", line.unwrap());
+    }
+
+    let output2 = File::create("rand.txt");
+    let output2 = match output2 {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("rand.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Can't create file: {:?}", error),
+            },
+            _other_error => panic!("Problem opening file: {:?}", error),
+        },
+    };
 }
