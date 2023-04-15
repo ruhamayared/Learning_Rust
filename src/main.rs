77f5da43557fb138ee7e5d3aa584903123bf3ba2
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-15 18:36:42 -0400

    Thread

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,21 @@ use std::f32::consts::PI;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
-
-mod restaurant;
-use crate::restaurant::order_food;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let b_int1 = Box::new(10);
-    println!("b_int1 = {}", b_int1);
-
-    struct TreeNode<T> {
-        pub left: Option<Box<TreeNode<T>>>,
-        pub right: Option<Box<TreeNode<T>>>,
-        pub key: T,
-    }
-    impl<T> TreeNode<T> {
-        pub fn new(key: T) -> Self {
-            TreeNode {
-                left: None,
-                right: None,
-                key,
-            }
+    let thread1 = thread::spawn(|| {
+        for i in 1..25 {
+            println!("Spawned thread: {}", i);
+            thread::sleep(Duration::from_millis(1));
         }
+    });
 
-        pub fn left(mut self, node: TreeNode<T>) -> Self {
-            self.left = Some(Box::new(node));
-            self
-        }
-
-        pub fn right(mut self, node: TreeNode<T>) -> Self {
-            self.right = Some(Box::new(node));
-            self
-        }
+    for i in 1..20 {
+        println!("Main thread: {}", i);
+        thread::sleep(Duration::from_millis(1));
     }
 
-    let node1 = TreeNode::new(1).left(TreeNode::new(2));
-    let node1 = TreeNode::new(1).right(TreeNode::new(3));
+    thread1.join().unwrap();
 }
