9fde9d5ce3d977cadbe03e092a9da47526eaa968
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-14 21:46:27 -0400

    traits

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,50 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::f32::consts::PI;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    struct Customer {
-        name: String,
-        address: String,
-        balance: f32,
+    const PI: f32 = 3.141592;
+
+    trait Shape {
+        fn new(length: f32, width: f32) -> Self;
+        fn area(&self) -> f32;
     }
-    let mut bob: Customer = Customer {
-        name: String::from("Bob Smith"),
-        address: String::from("555 Main St"),
-        balance: 234.50,
-    };
-    bob.address = String::from("505 Main Street")
+
+    struct Rectangle {
+        length: f32,
+        width: f32,
+    }
+
+    struct Circle {
+        length: f32,
+        width: f32,
+    }
+
+    impl Shape for Rectangle {
+        fn new(length: f32, width: f32) -> Rectangle {
+            return Rectangle { length, width };
+        }
+        fn area(&self) -> f32 {
+            return self.length * self.width;
+        }
+    }
+
+    impl Shape for Circle {
+        fn new(length: f32, width: f32) -> Circle {
+            return Circle { length, width };
+        }
+        fn area(&self) -> f32 {
+            return (self.length / 2.0).powf(2.0) * PI;
+        }
+    }
+
+    let rec: Rectangle = Shape::new(10.0, 10.0);
+    let circ: Circle = Shape::new(10.0, 10.0);
+
+    println!("Rec Area: {}", rec.area());
+    println!("Circ Area: {}", circ.area());
 }
