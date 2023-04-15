1a94a98c8bc2e4b59a4bfaf85085f091a7e9834d
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-14 21:27:41 -0400

    HashMaps

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,26 @@
 use rand::Rng;
 // use std::arch::aarch64::ST;
 use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
-fn print_str(x: String) {
-    println!("A string {}", x);
-}
-
-fn print_return_str(x: String) -> String {
-    println!("A string {}", x);
-    x
-}
-
-fn change_string(name: &mut String) {
-    name.push_str(" is happy!");
-    println!("Message: {}", name)
-}
-
 fn main() {
-    let mut str1 = String::from("Ruhama");
-    change_string(&mut str1)
+    let mut heroes = HashMap::new();
+    heroes.insert("Superman", "Clark Kent");
+    heroes.insert("Batman", "Bruce Wayne");
+    heroes.insert("The Flash", "Barry Allen");
+
+    for (k, v) in heroes.iter() {
+        println!("{} = {}", k, v)
+    }
+
+    if heroes.contains_key(&"Batman") {
+        let the_batman = heroes.get(&"Batman");
+        match the_batman {
+            Some(x) => println!("Batman is a hero!"),
+            None => println!("Batman is not a hero."),
+        }
+    }
 }
